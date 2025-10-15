2a13399eb5729f3e357aa88143adadcd49ac4ed8
abhiram-ar <abhiram2001.ars@gmail.com>
2025-10-15 13:58:44 +0530

    add microservice repo clone script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,22 @@ set -o pipefail
 # git clone -b develop https://github.com/CrossroadsAcademy/togather-user-service.git
 
 # --- CONFIG ---
-ROOT_DIR="$(pwd)/togather-dev"
+ROOT_DIR="$(pwd)/../togather-dev"
+
+   # TODO: need to add elastic search proxy
 REPOS=(
+  # "https://github.com/CrossroadsAcademy/togather-infra.git"
+  "https://github.com/CrossroadsAcademy/togather-user-service.git"
+  "https://github.com/CrossroadsAcademy/togather-location-service.git"
+  "https://github.com/CrossroadsAcademy/togather-experience-service.git"
   "https://github.com/CrossroadsAcademy/togather-websocket-service.git"
+  "https://github.com/CrossroadsAcademy/togather-chat-service.git"
   "https://github.com/CrossroadsAcademy/togather-auth-service.git"
+  "https://github.com/CrossroadsAcademy/togather-booking-finance-service.git"
+  "https://github.com/CrossroadsAcademy/togather-feed-service.git"
+  "https://github.com/CrossroadsAcademy/togather-graphql-service.git"
+  "https://github.com/CrossroadsAcademy/togather-notification-service.git" 
+  "https://github.com/CrossroadsAcademy/togather-partner-service.git"
 )
 
 # --- COLORS ---
@@ -26,8 +38,9 @@ cd "$ROOT_DIR"
 log "Cloning repositories..."
 for repo in "${REPOS[@]}"; do
   name=$(basename "$repo" .git)
+  log "Cloning $name"
   if [ ! -d "$name" ]; then
-    git clone "$repo"
+    git clone -b develop "$repo"
   else
     log "$name already exists, manually pull the latest changes..."
    #  (cd "$name" && git pull)
@@ -38,10 +51,10 @@ log "Installing dependencies..."
 # Example: Install Skaffold, Docker, etc.
 if ! command -v skaffold &>/dev/null; then
   log "Installing Skaffold..."
-  curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+  curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
   sudo install skaffold /usr/local/bin/
 fi
 
-log "Starting orchestration..."
-cd infra
-skaffold dev
+# log "Starting orchestration..."
+# cd infra
+# skaffold dev
