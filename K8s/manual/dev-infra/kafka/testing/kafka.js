f0c45298cbc97b993da3e73e5486507eb27c2ca4
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-10-17 15:35:12 +0530

    feat(kafka): Added kafka auth config files
    this commit adds shell files that can create tls creds and scarm creds and a auth setup sciprt that can add SCRAM user to kafka running inside teh docker  and a testing server file that can be used to test the auth setup

---
+++ K8s/manual/dev-infra/kafka/testing/kafka.js
@@ -0,0 +1,37 @@
+const { Kafka } = require("kafkajs");
+const fs = require("fs");
+
+// Kafka broker config
+const kafka = new Kafka({
+  clientId: "my-app",
+  //make sure to put certificate name here while connecting
+  //localhost and broker name mentioned on kafka-broker.txt is added
+  brokers: ["localhost:39093"],
+  ssl: {
+    rejectUnauthorized: true,
+    ca: [fs.readFileSync("../ca/ca_public_cert.crt", "utf-8")],
+  },
+  sasl: {
+    mechanism: "scram-sha-256",
+    username: "username added used setup auth sh",
+    password: "password added used  setup auth sh",
+  },
+  // hosted username and password is in infisical
+});
+
+const producer = kafka.producer();
+
+async function run() {
+  await producer.connect();
+  console.log("Connected to Kafka With SCRAM!");
+
+  await producer.send({
+    topic: "test-topic",
+    messages: [{ value: "Hello Kafka from Node.js!" }],
+  });
+
+  console.log("Message sent!");
+  await producer.disconnect();
+}
+
+run().catch(console.error);
